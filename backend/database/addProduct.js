8f4565c3146c7c8b087106ec7bd7b00824cd4cff
megashrieks <shrikanthbudya@gmail.com>
2018-10-19 21:34:54 +0530

    bug fix

--- backend/database/addProduct.js
+++ backend/database/addProduct.js
@@ -1,6 +1,6 @@
 let connection = require("./connection");
 module.exports = (details, callback) => {
-	let { name, price, count, detailinfo } = details;
+	let { name, price, count, details: detailinfo } = details;
 	connection.query(
 		`
 			Insert into Products(prod_name,prod_price,prod_count,prod_details)
