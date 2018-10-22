029246e65499a5003ebe632a8430d71e281dcd5b
megashrieks <shrikanthbudya@gmail.com>
2018-10-22 19:35:48 +0530

    bug fix and backend connection change

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,8 +1,9 @@
 let mysql = require("mysql");
+let { username, password } = require("../../secret");
 let conn = mysql.createConnection({
 	host: "localhost",
-	user: "root",
-	password: "",
+	user: username,
+	password: password,
 	database: "online_shopping_portal"
 });
 module.exports = conn;
--- backend/database/search.js
+++ backend/database/search.js
@@ -1,6 +1,11 @@
 let connection = require("./connection");
 module.exports = (keyword, options, callback) => {
-	let parameters = [options.count || 0, options.lprice, options.rprice];
+	let parameters = [
+		options.count || 0,
+		options.count || 0,
+		options.lprice,
+		options.rprice
+	];
 	let constraint = ` and prod_price
 		between ? and ?`;
 	if (!(options.lprice && options.rprice)) {
@@ -8,8 +13,14 @@ module.exports = (keyword, options, callback) => {
 		parameters.length -= 2;
 	}
 	connection.query(
-		`Select * from Products where prod_name like '%${keyword}%'
-		and prod_count >= ?` + constraint,
+		`Select * from Products P where prod_name like '%${keyword}%' 
+			and ((select P.prod_count - sum(O.count) from Orders O where O.prod_id=P.prod_id) >= ?
+			or(
+				(select count(*) from Orders O where O.prod_id=P.prod_id) = 0
+					and
+				P.prod_count >= ?
+			))
+		` + constraint,
 		parameters,
 		callback
 	);
