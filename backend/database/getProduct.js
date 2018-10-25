4179a7d8d911452a7dec7aadf6f4aeddc3aae270
megashrieks <shrikanthbudya@gmail.com>
2018-10-25 21:52:25 +0530

    bug fix in backend sql query

--- backend/database/getProduct.js
+++ backend/database/getProduct.js
@@ -3,7 +3,7 @@ let connection = require("./connection");
 module.exports = (prod_id, callback) => {
 	connection.query(
 		`
-		Select *,P.prod_count-(select count(*) from Orders O where O.prod_id=P.prod_id) as remaining from Products P where prod_id = ?
+		Select *,P.prod_count-(select coalesce(sum(O.count),0) from Orders O where O.prod_id=P.prod_id) as remaining from Products P where prod_id = ?
 	`,
 		prod_id,
 		callback
--- backend/database/search.js
+++ backend/database/search.js
@@ -1,11 +1,6 @@
 let connection = require("./connection");
 module.exports = (keyword, options, callback) => {
-	let parameters = [
-		options.count || 0,
-		options.count || 0,
-		options.lprice,
-		options.rprice
-	];
+	let parameters = [options.count || 0, options.lprice, options.rprice];
 	let constraint = ` and prod_price
 		between ? and ?`;
 	if (!(options.lprice && options.rprice)) {
@@ -13,13 +8,10 @@ module.exports = (keyword, options, callback) => {
 		parameters.length -= 2;
 	}
 	connection.query(
-		`Select *,P.prod_count-(select count(*) from Orders OO where OO.prod_id=P.prod_id) as remaining from Products P where prod_name like '%${keyword}%' 
-			and ((select P.prod_count - sum(O.count) from Orders O where O.prod_id=P.prod_id) >= ?
-			or(
-				(select count(*) from Orders O where O.prod_id=P.prod_id) = 0
-					and
-				P.prod_count >= ?
-			))
+		`Select *,P.prod_count-
+			(select coalesce(sum(O.count),0) from Orders O where O.prod_id=P.prod_id)
+			as remaining from Products P where P.prod_name like
+			'%${keyword}%' group by P.prod_id having remaining >= ?
 		` + constraint,
 		parameters,
 		callback
